36dd3efda3e50f9bd02684d48b58745ccc8fe5d1
Ali Salmeh <alisalmeh18@gmail.com>
2023-08-12 11:23:19 +0330

    add initial commit

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace AliSalmeh_ProjectWeek5.MiniGame_Refactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mini Game
+
+            Random random = new Random();
+            bool playAgain = true;
+
+            do
+            {
+                Console.Clear();
+
+                int computerRandomNum = random.Next(1, 101);
+                int userGuess = 0;
+                int round = 0;
+
+                while (userGuess != computerRandomNum)
+                {
+                    Console.WriteLine("Guess a number between 1 - 100 :");
+                    userGuess = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine($"Guess: {userGuess}");
+
+                    if (userGuess > 100 || userGuess < 0)
+                    {
+                        Console.WriteLine("You entered invalid number!!");
+                    }
+                    else if (userGuess < computerRandomNum)
+                    {
+                        Console.WriteLine($"{userGuess} is too low!");
+                    }
+                    else if (userGuess > computerRandomNum)
+                    {
+                        Console.WriteLine($"{userGuess} is too high!");
+                    }
+                    round++;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("YOU WIN *__*");
+                Console.WriteLine($"Round: {round}");
+                Console.WriteLine();
+                Console.WriteLine("Would you like to play again? (Y/N): ");
+
+                var userAns = Console.ReadLine().ToUpper();
+                playAgain = userAns == "Y" ? true : false;
+
+            } while (playAgain);
+
+            Console.WriteLine("Thanks for playing! ... ^__^");
+        }
+    }
+}
